96a8b55483de3b1e00b79e6d4680df6539b8a999
Roman <romantolstyaklitvinenko@gmail.com>
2023-08-08 20:30:34 +0600

    добавлено модальное окно

--- frontend/src/pages/components/layout/Layout.tsx
+++ frontend/src/pages/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import ScrollToTopButton from "../ScrollToTopButton/ScrollToTopButton";
